e8ffa6c1b73768f7de1d9921c087ceb93cd5b1fc
kepplertreet <saranshmazumder98@gmail.com>
2024-09-28 19:51:04 +0530

    Updates 28th sept

--- init.lua
+++ init.lua
@@ -613,7 +613,7 @@ require('lazy').setup({
   require 'kickstart.plugins.lualine',
   require 'kickstart.plugins.none-ls',
   require 'kickstart.plugins.autopairs',
-  --[[   require 'kickstart.plugins.lint', ]]
+  require 'kickstart.plugins.lint',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
