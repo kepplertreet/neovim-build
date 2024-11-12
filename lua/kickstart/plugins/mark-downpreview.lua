a2c5733e3e19e841717c3ee31932dc6322808399
kepplertreet <saranshmazumder98@gmail.com>
2024-11-12 19:40:34 +0530

    wow this is the commit

--- lua/kickstart/plugins/mark-downpreview.lua
+++ lua/kickstart/plugins/mark-downpreview.lua
@@ -1,18 +1,18 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
 
-	ft = "markdown",
-	build = function()
-		vim.fn["mkdp#util#install"]()
-	end,
-	config = function()
-		vim.g.mkdp_browser = "open -a 'Google Chrome'"
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "markdown",
-			callback = function()
-				vim.cmd("MarkdownPreview")
-			end,
-		})
-	end,
+  ft = 'markdown',
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
+  config = function()
+    vim.g.mkdp_browser = 'brave-browser'
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'markdown',
+      callback = function()
+        vim.cmd 'MarkdownPreview'
+      end,
+    })
+  end,
 }
